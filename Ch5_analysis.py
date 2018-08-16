b66e0e02af8cab1a94f4f3ef4fa33ef1fdbb0638
KP <kp259@le.ac.uk>
2018-08-16 20:45:25 +0100

    File extraction info

--- Ch5_analysis.py
+++ Ch5_analysis.py
@@ -11,3 +11,19 @@ Chapter 5 - Distraction and photometry in NAc terminals
 
 
 """
+
+## NAPH
+## ? Do I need to separate these by core and shell? Enough in each category? > 
+# Lick day
+TDTfiles_naph_lick = ['NAPH02_licktrain', 'NAPH03_licktrain', 'NAPH04_licktrain', 'NAPH05_licktrain',\
+                      'NAPH07_licktrain','NAPH08_licktrain','NAPH09_licktrain','NAPH10_licktrain']
+# Distraction day 
+TDTfiles_naph_dis = ['NAPH02_distraction', 'NAPH03_distraction', 'NAPH04_distraction', 'NAPH05_distraction',\
+                     'NAPH07_distraction', 'NAPH08_distraction', 'NAPH09_distraction', 'NAPH10_distraction']
+# Habituation day 
+TDTfiles_naph_hab = ['NAPH02_habituation', 'NAPH03_habituation', 'NAPH04_habituation',\
+                     'NAPH07_habituation', 'NAPH08_habituation', 'NAPH09_habituation', 'NAPH10_habituation']
+
+
+TDTfilepath = '/Volumes/KP_HARD_DRI/All_Matlab_Converts/BIG CONVERSION 14 AUG 2018/NAPH matfiles/'
+
--- Ch6_analysis.py
+++ Ch6_analysis.py
@@ -9,3 +9,15 @@ Created on Fri May 25 09:10:14 2018
 """
 Chapter 6 - Distraction and photometry in VTA of TH:Cre's 
 """
+
+# cTHPH - 3 rats only with all data (lick and distraction) - maybe add all rats with signal to 
+# the lick files, as much data as possible from the cohort 
+# Lick day 
+TDTfiles_cTHPH_lick = ['cTHPH1-03_lick4', 'cTHPH1-05_lick4', 'cTHPH1-09_lick4']
+# Distraction day 
+TDTfiles_cTHPH_dis = ['cTHPH1-03_distraction', 'cTHPH1-05_distraction', 'cTHPH1-09_distraction']
+# Habituation day  - no habituation days for this cohort
+TDTfiles_cTHPH_hab = []
+
+TDTfilepath = '/Volumes/KP_HARD_DRI/All_Matlab_Converts/BIG CONVERSION 14 AUG 2018/cTHPH matfiles/'
+
