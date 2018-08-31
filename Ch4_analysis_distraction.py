17cabb43c719fe4f5baefe8baa5df650d6498913
KP <kp259@le.ac.uk>
2018-08-31 10:52:41 +0100

    Percent distracted for barscattter

--- Ch4_analysis_distraction.py
+++ Ch4_analysis_distraction.py
@@ -5,5 +5,488 @@ Created on Mon Aug 20 20:17:44 2018
 
 @author: u1490431
 """
+def MetaExtractorTHPH (metafile):
+    f = open(metafile, 'r')
+    f.seek(0)
+    Metafilerows = f.readlines()[1:]
+    tablerows = []
 
-sss
+    for row in Metafilerows: 
+        items = row.split(',')
+        tablerows.append(items)
+
+    TDTFile, MedFilenames, RatID, Date, Session, Include, Licks, Totdistractors, Distracted, \
+    Percentdistracted, Note, Endcol = [],[],[],[],[],[],[],[],[],[],[],[]
+
+    for i, lst in enumerate(tablerows):
+        
+        
+        
+       TDTFile = TDTFile + [lst[0]]
+       MedFilenames = MedFilenames + [lst[1]]
+       RatID = RatID + [lst[2]]
+       Date = Date + [lst[3]]
+       Session = Session + [lst[4]]
+       Include = Include + [lst[5]]
+       Licks = Licks + [lst[6]]
+       Totdistractors = Totdistractors + [lst[7]]  
+       Distracted = Distracted + [lst[8]]
+       Percentdistracted = Percentdistracted + [lst[9]]
+       Note = Note + [lst[10]]
+       Endcol = Endcol + [lst[11]]
+
+ 
+    return ({'MedFilenames':MedFilenames, 'RatID':RatID, 'Date':Date, 'Session':Session, \
+             'Include':Include, 'Licks':Licks, 'PercentDistracted':Percentdistracted})
+
+
+
+def subsetter2(dictionary, dates, dis=False, verbose=False):
+    '''
+    SUBSETTER KP
+    # Subsets data according to date, reads in dictionnary produced from metafile
+    # and subsets into variable based on date(s) and drug condition 
+    # if distraction day argument is given as True adds the distractor type 
+    # to the output lists for later processing 
+    
+    Adapted to include & include == 1 (because not all had the same date for last lick day)
+    In metafile do not include 2.7 and 2.8 on the lick 3 day but do include them on lick 6
+     
+    
+    '''
+    subset = []
+    
+    for ind, filename in enumerate(dictionary['MedFilenames']):
+        path = medfolder + filename
+        onsets, offsets, med_dis_times, dis_type = medfilereader(path, ['b', 'c', 'i', 'j'], remove_var_header = True)  # e onset, f offset
+
+        if dis == True:
+            if dictionary['Date'][ind] in dates and dictionary['Include'][ind] == '1':
+                subset.append([onsets, offsets, dis_type, dictionary['RatID'][ind]])
+                
+        elif dis==False:   
+            if dictionary['Date'][ind] in dates and dictionary['Include'][ind] == '1':
+                subset.append([onsets, offsets, dictionary['RatID'][ind]])
+            
+        if verbose: #assumes true
+            print('filename, or comment ...') 
+    return subset
+
+
+
+def percentdisgroup(distractiondict):
+    ''' Discalc_sal_M == distractiondict '''
+    
+    percent_dis_group = []
+    for rat in distractiondict: 
+        percentage = len(rat[0]) / (len(rat[0])+len(rat[1])) * 100
+        percent_dis_group.append(percentage)
+    return percent_dis_group
+
+
+
+def disbygroup(dictionary):
+    ''' Prodcues times of distracted and not distracted as 2 lists
+    takes a dictionary of grouped rat data 
+    '''
+
+    dis = []
+    for rat in dictionary:
+        
+        discalc = distractionCalc2(rat[0])         
+        distracted, notdistracted = distractedOrNot(discalc, rat[0])
+        dis.append([distracted, notdistracted])
+        
+    return dis
+
+# Functions 
+
+"""
+This function will create bar+scatter plots when passed a 1 or 2 dimensional
+array. Data needs to be passed in as a numpy object array, e.g.
+data = np.empty((2), dtype=np.object)
+data[0] = np.array(allData['nCasLicks'][index])
+data[1] = np.array(allData['nMaltLicks'][index])
+Various options allow specification of colors and paired/unpaired plotting.
+It can return the figures, axes, bars, and scatters for further modification.
+e.g.
+fig1, ax1, barlist1, sc1 = jmf.barscatter(data)
+for i in barlist1[1].get_children():
+    i.set_color('g')
+"""
+def barscatter(data, transpose = False, unequal=False,
+                groupwidth = .75,
+                barwidth = .9,
+                paired = False,
+                spaced = False,
+                barfacecoloroption = 'same', # other options 'between' or 'individual'
+                barfacecolor = ['white'],
+                baredgecoloroption = 'same',
+                baredgecolor = [''],
+                baralpha = 1,
+                scatterfacecoloroption = 'same',
+                scatterfacecolor = ['white'],
+                scatteredgecoloroption = 'same',
+                scatteredgecolor = ['grey'],
+                scatterlinecolor = 'grey', # Don't put this value in a list
+                scattersize = 80,
+                scatteralpha = 1,
+                linewidth=1,
+                ylabel = 'none',
+                xlabel = 'none',
+                grouplabel = 'auto',
+                itemlabel = 'none',
+                barlabels = [],
+                barlabeloffset=0.1,
+                grouplabeloffset=0.2,
+                yaxisparams = 'auto',
+                show_legend = 'none',
+                xrotation=0,
+                legendloc='upper right',
+                ax=[]):
+
+    if unequal == True:
+        dims = np.ndim(data)
+        data_obj = np.ndarray((np.shape(data)), dtype=np.object)
+        for i1, dim1 in enumerate(data):
+            for i2, dim2 in enumerate(dim1):
+                data_obj[i1][i2] = np.array(dim2, dtype=np.object)
+        data = data_obj
+    
+    if type(data) != np.ndarray or data.dtype != np.object:
+        dims = np.shape(data)
+        if len(dims) == 2 or len(dims) == 1:
+            data = data2obj1D(data)
+
+        elif len(dims) == 3:
+            data = data2obj2D(data)
+              
+        else:
+            print('Cannot interpret data shape. Should be 2 or 3 dimensional array. Exiting function.')
+            return
+
+    # Check if transpose = True
+    if transpose == True:
+        data = np.transpose(data)
+        
+    # Initialize arrays and calculate number of groups, bars, items, and means
+    
+    barMeans = np.zeros((np.shape(data)))
+    items = np.zeros((np.shape(data)))
+    
+    nGroups = np.shape(data)[0]
+    groupx = np.arange(1,nGroups+1)
+
+    if len(np.shape(data)) > 1:
+        grouped = True
+        barspergroup = np.shape(data)[1]
+        barwidth = (barwidth * groupwidth) / barspergroup
+        
+        for i in range(np.shape(data)[0]):
+            for j in range(np.shape(data)[1]):
+                barMeans[i][j] = np.nanmean(data[i][j])
+                items[i][j] = len(data[i][j])
+        
+    else:
+        grouped = False
+        barspergroup = 1
+        
+        for i in range(np.shape(data)[0]):
+            barMeans[i] = np.nanmean(data[i])
+            items[i] = len(data[i])
+    
+    # Calculate x values for bars and scatters
+    
+    xvals = np.zeros((np.shape(data)))
+    barallocation = groupwidth / barspergroup
+    k = (groupwidth/2) - (barallocation/2)
+    
+    if grouped == True:
+        
+        for i in range(np.shape(data)[0]):
+            xrange = np.linspace(i+1-k, i+1+k, barspergroup)
+            for j in range(barspergroup):
+                xvals[i][j] = xrange[j]
+    else:
+        xvals = groupx
+    
+    # Set colors for bars and scatters
+     
+    barfacecolorArray = setcolors(barfacecoloroption, barfacecolor, barspergroup, nGroups, data)
+    baredgecolorArray = setcolors(baredgecoloroption, baredgecolor, barspergroup, nGroups, data)
+     
+    scfacecolorArray = setcolors(scatterfacecoloroption, scatterfacecolor, barspergroup, nGroups, data, paired_scatter = paired)
+    scedgecolorArray = setcolors(scatteredgecoloroption, scatteredgecolor, barspergroup, nGroups, data, paired_scatter = paired)
+    
+    # Initialize figure
+    if ax == []:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+    
+    # Make bars
+    barlist = []
+    barx = []
+    for x, y, bfc, bec in zip(xvals.flatten(), barMeans.flatten(),
+                              barfacecolorArray, baredgecolorArray):
+        barx.append(x)
+        barlist.append(ax.bar(x, y, barwidth,
+                         facecolor = bfc, edgecolor = bec,
+                         zorder=-1))
+    
+    # Uncomment these lines to show method for changing bar colors outside of
+    # function using barlist properties
+    #for i in barlist[2].get_children():
+    #    i.set_color('r')
+    
+    # Make scatters
+    sclist = []
+    if paired == False:
+        for x, Yarray, scf, sce  in zip(xvals.flatten(), data.flatten(),
+                                        scfacecolorArray, scedgecolorArray):
+            for y in Yarray:
+                if spaced == True:
+                    sclist.append(ax.scatter(x+np.random.random(size=1)*barallocation, y, s = scattersize,
+                             c = scf,
+                             edgecolors = sce,
+                             zorder=20))
+                else:
+                     sclist.append(ax.scatter(x, y, s = scattersize,
+                                     c = scf,
+                                     edgecolors = sce,
+                                     zorder=20))
+
+    elif grouped == True:
+        for x, Yarray, scf, sce in zip(xvals, data, scfacecolorArray, scedgecolorArray):
+            for y in np.transpose(Yarray.tolist()):
+                sclist.append(ax.plot(x, y, '-o', markersize = scattersize/10,
+                         color = scatterlinecolor,
+                         linewidth=linewidth,
+                         markerfacecolor = scf,
+                         markeredgecolor = sce,
+                         zorder=20))
+    elif grouped == False:
+        for n,_ in enumerate(data[0]):
+            y = [y[n-1] for y in data]
+            sclist.append(ax.plot(xvals, y, '-o', markersize = scattersize/10,
+                         color = scatterlinecolor,
+                         linewidth=linewidth,
+                         markerfacecolor = scfacecolorArray[0],
+                         markeredgecolor = scedgecolorArray[0],
+                         zorder=20))
+    
+    # Label axes
+    if ylabel != 'none':
+        plt.ylabel(ylabel)
+    
+    if xlabel != 'none':
+        plt.xlabel(xlabel)
+    
+    # Set range and tick values for Y axis
+    if yaxisparams != 'auto':
+        ax.set_ylim(yaxisparams[0])
+        plt.yticks(yaxisparams[1])
+       
+    # X ticks
+    ax.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off') # labels along the bottom edge are off
+
+    if grouplabel == 'auto':
+        plt.tick_params(labelbottom='off')
+    else:
+        if len(barlabels) > 0:
+            plt.tick_params(labelbottom='off')
+            yrange = ax.get_ylim()[1] - ax.get_ylim()[0]
+            offset = ax.get_ylim()[0] - yrange*grouplabeloffset
+            for idx, label in enumerate(grouplabel):
+                ax.text(idx+1, offset, label, va='top', ha='center')
+        else:
+            plt.xticks(range(1,nGroups+1), grouplabel)
+        
+    if len(barlabels) > 0:
+        if len(barlabels) != len(barx):
+            print('Wrong number of bar labels for number of bars!')
+        else:
+            yrange = ax.get_ylim()[1] - ax.get_ylim()[0]
+            offset = ax.get_ylim()[0] - yrange*barlabeloffset
+            for x, label in zip(barx, barlabels):
+                ax.text(x, offset, label, va='top', ha='center',rotation=xrotation)
+    
+    # Hide the right and top spines and set bottom to zero
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_position('zero')
+    
+    if show_legend == 'within':
+        if len(itemlabel) != barspergroup:
+            print('Not enough item labels for legend!')
+        else:
+            legendbar = []
+            legendtext = []
+            for i in range(barspergroup):
+                legendbar.append(barlist[i])
+                legendtext.append(itemlabel[i])
+            plt.legend(legendbar, legendtext, loc=legendloc)
+    
+    return ax, barx, barlist, sclist
+
+#plt.savefig('foo.png')
+        
+# To do
+# check if n's are the same for paired and if not default to unpaired
+# add color options for scatters
+# add alpha options etc
+# add axis options
+# remove white background
+# work out how to export or save as pdf, tiff, eps etc
+# work out how to return handles to scatters so can be altered outside of function
+# make help doc
+# make html file to show usage using ijupyt
+      
+def setcolors(coloroption, colors, barspergroup, nGroups, data, paired_scatter = False):
+            
+    nColors = len(colors)
+    
+    if (paired_scatter == True) & (coloroption == 'within'):
+        print('Not possible to make a Paired scatter plot with Within setting.')
+        coloroption = 'same'
+        
+    if coloroption == 'within':
+        if nColors < barspergroup:
+            print('Not enough colors for this option! Reverting to one color.')
+            coloroption = 'same'
+        elif nColors > barspergroup:
+            colors = colors[:barspergroup]
+        coloroutput = [colors for i in data]
+        coloroutput = list(chain(*coloroutput))
+        
+    if coloroption == 'between':
+        if nColors < nGroups:
+            print('Not enough colors for this option! Reverting to one color.')
+            coloroption = 'same'
+        elif nColors > nGroups:
+            colors = colors[:nGroups]
+        if paired_scatter == False:
+            coloroutput = [[c]*barspergroup for c in colors]
+            coloroutput = list(chain(*coloroutput))
+        else:
+            coloroutput = colors
+            
+    if coloroption == 'individual':
+        if nColors < nGroups*barspergroup:
+            print('Not enough colors for this color option')
+            coloroption = 'same'
+        elif nColors > nGroups*barspergroup:
+            coloroutput = colors[:nGroups*barspergroup]
+        else: 
+            coloroutput = colors
+    
+    if coloroption == 'same':
+        coloroutput = [colors[0] for x in range(len(data.flatten()))]
+
+    return coloroutput
+
+def data2obj1D(data):
+    obj = np.empty(len(data), dtype=np.object)
+    for i,x in enumerate(data):
+        obj[i] = np.array(x)  
+    return obj
+
+def data2obj2D(data):
+    obj = np.empty((np.shape(data)[0], np.shape(data)[1]), dtype=np.object)
+    for i,x in enumerate(data):
+        for j,y in enumerate(x):
+            obj[i][j] = np.array(y)
+    return obj
+
+
+
+
+################################################################################################
+################################################################################################
+
+## BEHAVIOUR SUBSETTING, PERCENT DISTRACTED AND PLOTS [THPH1, THPH2]
+
+metafile = '/Volumes/KP_HARD_DRI/kp259/THPH1AND2/THPH1&2Metafile.csv'
+extract_data = MetaExtractorTHPH(metafile)
+# Folder with all medfiles (THPH1 and THPH2)
+medfolder = '/Volumes/KP_HARD_DRI/kp259/THPH1AND2/med/'
+
+
+
+''' 
+  Info DPCP1(16) and DPCP2(16) males, DPCP3(24) females :
+      
+  THPH1      |THPH2.1 - 2.6|THPH2.7 - 2.8|CONDITION
+  -----------|-------------|-------------|-----------
+  170620     |170809       |170814       |last lick
+  170621     |170810       |170815       |dis
+  170622     |170811       |170816       |hab 1
+  
+  All included, can later only subset the first 12 rats and ignore the 
+  unequal numbers from no habituation on rat 2.8
+
+'''
+
+
+last_lick = subsetter2(extract_data, ['170620', '170809', '170814'])
+distraction= subsetter2(extract_data, ['170621', '170810', '170815'], dis=True)
+habituation = subsetter2(extract_data, ['170622', '170811', '170806'], dis=True)
+
+
+discalcLick = []
+discalcDis = []
+discalcHab = []
+
+for rat in last_lick:
+    discalc = distractionCalc2(rat[0])
+    distracted, notdistracted = distractedOrNot(discalc, rat[0])
+    discalcLick.append([distracted, notdistracted])
+
+for rat in distraction:
+    discalc = distractionCalc2(rat[0])
+    distracted, notdistracted = distractedOrNot(discalc, rat[0])
+    discalcDis.append([distracted, notdistracted])
+    
+for rat in habituation:
+    discalc = distractionCalc2(rat[0])
+    distracted, notdistracted = distractedOrNot(discalc, rat[0])
+    discalcHab.append([distracted, notdistracted])    
+
+percentdisLick = []
+percentdisDis = []
+percentdisHab = [] 
+
+for rat in discalcLick:
+    percent = len(rat[0]) / (len(rat[0])+len(rat[1])) * 100
+    percentdisLick.append(percent)
+
+for rat in discalcDis:
+    percent = len(rat[0]) / (len(rat[0])+len(rat[1])) * 100
+    percentdisDis.append(percent)
+    
+for rat in discalcHab:
+    percent = len(rat[0]) / (len(rat[0])+len(rat[1])) * 100
+    percentdisHab.append(percent)    
+    
+
+##########################################################################################
+
+# PERCENT DISTRACTED - THPH1 AND THPH2
+    
+# For barscatter plots excluded rats 2.7 and 2.8 (as not all point on all days)
+data = [[percentdisLick[0:12], percentdisDis[0:12], percentdisHab]]
+
+col3 = ['#FFE5A5','#FFE5A5','#FFE5A5']
+col3 = ['#a2e283','#4cbb17','#257200']
+labels = ['mod', 'dis', 'hab']
+mpl.rcParams['font.size'] = 14
+figureA, ax = plt.subplots(nrows=1, ncols=1, figsize=(4,5)) ### x,y
+ax, barx, barlist, sclist = barscatter(data, transpose=False, ax=ax,paired=True, barfacecolor=col3, barlabels=labels,barfacecoloroption='individual',  ylabel='Percent distracted (%)', itemlabel=['1','2'], barlabeloffset=0.05) #,grouplabel=['Sal', 'Pcp', 'day -2', 'day -1'])
+ax.spines['bottom'].set_visible(False)
+#figureA.savefig('/Volumes/KPMSB352/Thesis/Chapter 4 - Photometry VTA/Figures/PercentDisBarScatter.pdf', bbox_inches="tight")
+
+   
+    
