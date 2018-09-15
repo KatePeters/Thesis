ffacb73424308bf063a9841f6ff7386079a6244c
KP <kp259@le.ac.uk>
2018-09-15 12:00:36 +0100

    Instructions added

--- Ch2_figures.py
+++ Ch2_figures.py
@@ -11,3 +11,36 @@ Chapter 2 Plotting figures
 
 
 """
+
+
+## Light blues and pinks
+
+# Light blue - sky 
+#81D4FA
+#4FC3F7  
+#03A9F4
+#0277BD 
+
+# Pinks 
+
+#F06292  
+#E91E63
+#880E4F
+'''
+
+Maybe means of different rats with distributions? 
+Bar scatter of ALL bursts, ALL runs, ALL etc... 
+8 Bars with all the data from each rat
+
+licks (last day) - all rats across 3 days (means with each rat as a point)
+number of bursts
+burst lengths
+other parameters?
+runs etc. 
+
+
+Percent distracted across days
+
+Stats --> ANOVA percent distracted mod, dis, hab1, hab2, sal, amph
+Sats --> PDPs ANOVA, dis,hab1,hab2,sal,amph
+'''
