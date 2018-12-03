9e29176a7a41cf396821a97e19cf06dd79528857
KP <kp259@le.ac.uk>
2018-12-03 15:46:52 +0000

    Fixing figure 2.5

---
+++ Ch2_Figure 2.5 across days.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 14:51:10 2018
+
+@author: u1490431
+"""
+
+lick_analysis7 = lickanalysis2(last_lick)
+
+lick_analysis6 = lickanalysis2(lick_minus1)
+
+lick_analysis5 = lickanalysis2(lick_minus2)
+
+lick_analysis4 = lickanalysis2(lick_minus3)
+
+lick_analysis3 = lickanalysis2(lick_minus4)
+
+lick_analysis2 = lickanalysis2(lick_minus5)
+
+lick_analysis1 = lickanalysis2(lick_minus6)
+
+# Make burst / cluster / interval measures across days and make bar scatters for these too 
